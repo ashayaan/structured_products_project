62c6bba94d64dcaf62e78c25236acc73913d42ca
ashayaan <a.shayaan@gmail.com>
2020-12-18 21:34:29 -0500

    latest

--- pricing.py
+++ pricing.py
@@ -2,13 +2,14 @@
 # @Author: shayaan
 # @Date:   2020-12-05 22:26:28
 # @Last Modified by:   shayaan
-# @Last Modified time: 2020-12-15 18:38:26
+# @Last Modified time: 2020-12-18 21:30:07
 
 import pandas as pd
 import numpy as np
 from pandas_datareader import data as pdr
 from scipy import stats
 import numpy as np
+import datetime 
 np.random.seed(12)
 
 class Pricing(object):
@@ -16,16 +17,14 @@ class Pricing(object):
 	def __init__(self, start_date:str, end_date:str) -> None:
 		'''
 		Constructor for the Pricing class
-	
 		Input: start_date string, end_date string
-
 		'''
 		super(Pricing, self).__init__()
 		self.start_date = start_date
 		self.end_date = end_date
 		self.stoxx = pdr.DataReader('^STOXX50E',start=self.start_date,end=self.end_date,data_source='yahoo').round(4)
 		self.eurusd = pdr.DataReader('EURUSD=X',start=self.start_date,end=self.end_date,data_source='yahoo')
-		self.libor_rate = pdr.DataReader('USD3MTD156N','fred',start=self.start_date,end=self.end_date)
+		self.libor_rate = pdr.DataReader('USD3MTD156N','fred',start=self.start_date,end=self.end_date)	
 		self.findReturns()
 
 	def findReturns(self) -> None:
@@ -66,11 +65,11 @@ class Pricing(object):
 			dem += (self.libor_rate.iloc[i]['USD3MTD156N'] - r_mean)**2
 
 		self.a = 1 - (num)/dem
-		self.b = r_mean + self.a * r_mean
+		self.b = r_mean - (1-self.a) * r_mean
 
 		s = 0
 		for i in range(1,len(self.libor_rate)):
-			s += (self.libor_rate.iloc[i]['USD3MTD156N'] - self.b + (1-self.a)*self.libor_rate.iloc[i-1]['USD3MTD156N'])**2
+			s += (self.libor_rate.iloc[i]['USD3MTD156N'] - self.b - (1-self.a)*self.libor_rate.iloc[i-1]['USD3MTD156N'])**2
 		
 		self.sigma = np.sqrt(s/(len(self.libor_rate)-2))
 
@@ -83,16 +82,14 @@ class Pricing(object):
 
 		#number of simulations for libor
 		for i in range(M):
-			r = self.libor_rate.iloc[-1]['USD3MTD156N']
+			r = self.libor_rate.iloc[-1]['USD3MTD156N'] 
 			for j in range((T - delta_t )):
 				weiner_process = np.random.normal(0,1)
-				# print("B : {} second : {} weiner : {} ".format(self.b,(1-self.a) * r,self.sigma * weiner_process ) ) 
 				r = self.b + (1-self.a) * r + self.sigma * weiner_process
-			print(r)
 			rates[i] = r
 		return rates
 
-	def estimatePrice(self, T:int, K:float , K_:float , M:int , stoxx_0:float, eurusd_0:float, forward_rate:float, quanto_0:float, delta_t=3)->float:
+	def estimatePrice(self, T:int, K:float , K_:float , M:int,forward_rate:float, delta_t=3)->float:
 		'''
 		Input
 		T : number of years as int
@@ -106,21 +103,28 @@ class Pricing(object):
 		'''
 
 		#Simulation Quanto
+
+		stoxx_0 = self.stoxx.iloc[-1]['Close']
+		eurusd_0 = self.eurusd.iloc[-1]['Close']
+		quanto_0 = stoxx_0 * eurusd_0
+
+		print("Stoxx : {} eurusd:{}".format(stoxx_0, eurusd_0))
+
 		W_1 = np.random.normal(0,T,M)
 		W_2 = np.random.normal(0,T,M)
 		quanto = stoxx_0 * eurusd_0 * np.exp((self.stoxx_mean * self.eurusd_mean - (self.stoxx_std**2 + self.eurusd_std**2)/2)*T + self.stoxx_std*W_1 + self.eurusd_std*W_2 )
-		
 		libor = self.simulateLIBOR(T,delta_t,M)
-		print(libor)
+		libor = np.where(libor>0,libor,0)
+
 		price = (quanto/quanto_0 -K ) * (libor/forward_rate - K_)
 		price = np.where( price > 0 , price,0 )
 
-		return np.mean(price)
+		return np.mean(price) /(1 + 0.00335)
 
 
 if __name__ == '__main__':
-	start_date = "2019-01-01"
-	end_date = "2019-12-12"
+	start_date = "2020-01-01"
+	end_date =  datetime.datetime.now().strftime('%Y-%m-%d')
 	test = Pricing(start_date,end_date)
 	test.estimateParameters()
-	print("Price of the option is:{} ".format(test.estimatePrice(1,100,101,100,1.2,100.0,10.1,10))) 
+	print("Price of the option is:{} ".format(test.estimatePrice(1, 0.01, 0.012, 100, 0.3)) ) 
